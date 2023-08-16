ac02693777e08cda54b06ca31b1ec6153126ea56
Victoria <adedayovicky123@gmail.com>
2023-08-16 16:35:00 +0100

    add comment to javascript code for better understanding

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const button = document.getElementById("btn");
 const acctInfo = document.querySelector(".accountInfo");
-console.log(acctInfo);
 
 button.addEventListener("click", function () {
-  // Change the content of the display element
   acctInfo.classList.toggle("show");
 });
 
@@ -11,14 +9,22 @@ let slideIndex = 0;
 
 function showSlides() {
   let slides = document.getElementsByClassName("mySlides");
+
+  // Hide all slides
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
+  // Move to the next slide
   slideIndex++;
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
+
+  // Display the current slide
   slides[slideIndex - 1].style.display = "block";
+
+  // Call the showSlides function again after 3 seconds
   setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
