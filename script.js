84b37e9644b65dc2bc68e00802bfd9a872e42ad9
Victoria <adedayovicky123@gmail.com>
2023-08-16 17:41:30 +0100

    Birthday prayers typewriter - done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Display text on button click
 const button = document.getElementById("btn");
 const acctInfo = document.querySelector(".accountInfo");
 
@@ -5,6 +6,7 @@ button.addEventListener("click", function () {
   acctInfo.classList.toggle("show");
 });
 
+// Slide show
 let slideIndex = 0;
 
 function showSlides() {
@@ -29,3 +31,52 @@ function showSlides() {
 }
 
 showSlides();
+
+// Typewriter
+const sentences = [
+  "May the LORD perfect ALL that concerns you.",
+  "May your day be filled with Joy, Love and Peace.",
+  "May the favors of God surround you like a protective shield.",
+  "May He make his face to shine upon you and be gracious to you.",
+  "May He lift up his countenance upon you and give you peace.",
+];
+
+const textElement = document.getElementById("text");
+const cursorElement = document.getElementById("cursor");
+
+let sentenceIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (sentenceIndex < sentences.length) {
+    const sentence = sentences[sentenceIndex];
+    if (charIndex < sentence.length) {
+      textElement.textContent += sentence.charAt(charIndex);
+      charIndex++;
+      setTimeout(type, 100); // Adjust typing speed here
+    } else {
+      cursorElement.style.display = "none";
+      setTimeout(clearText, 1000); // Delay before clearing
+    }
+  } else {
+    sentenceIndex = 0; // Restart from the first sentence
+    setTimeout(type, 1000); // Delay before starting again
+  }
+}
+
+function clearText() {
+  const currentSentence = sentences[sentenceIndex];
+  if (charIndex >= currentSentence.length) {
+    charIndex = 0;
+    sentenceIndex++;
+    textElement.textContent = "";
+    cursorElement.style.display = "inline-block";
+    setTimeout(type, 500); // Delay before typing next sentence
+  } else {
+    textElement.textContent = currentSentence.substring(0, charIndex);
+    charIndex++;
+    setTimeout(clearText, 100); // Adjust clearing speed here
+  }
+}
+
+type();
